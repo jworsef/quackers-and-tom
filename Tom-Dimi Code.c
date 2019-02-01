670ccd885540db2afed74d399022a8740c269fc9
TomGreenway2 <47110725+TomGreenway2@users.noreply.github.com>
2019-02-01 17:29:19 +0000

    Update Tom-Dimi Code.c

--- Tom-Dimi Code.c
+++ Tom-Dimi Code.c
@@ -4,52 +4,67 @@
 uint32_t LEDState = 0;
 
 void SysTick_Handler (void) {
-if (LEDState == 0) LEDState = 1;
-else if (LEDState == 1) LEDState = 2;
-else if (LEDState == 2) LEDState = 3;
-else LEDState = 0;
+	buttonDetect();
+		
+}
+	
+//if (LEDState == 0) LEDState = 1;
+//else if (LEDState == 1) LEDState = 2;
+//else if (LEDState == 2) LEDState = 3;
+//else LEDState = 0;
+//}
+
+void greenLED_only (){
+		GPIOD -> BSRR =   0xE0001000; //green LED
 }
 
-void greenLED_invert (){
-	if (GPIOD -> ODR == 0x0000F000) {
-		GPIOD -> ODR =   0x00000000; }//green LED
-	else{ GPIOD -> ODR =   0x0000F000;}
+void orangeLED_only (){
+	GPIOD -> BSRR =   0xD0002000;
 }
 
-void buttonDetect () {
-	if (GPIOA -> IDR == 0x00000001)
-	{greenLED_invert ();}
+void redLED_only (){
+	GPIOD -> BSRR =   0xB0004000;
+}
+
+void blueLED_only (){
+	GPIOD -> BSRR =   0x70008000;
+}
+
+int buttonDetect () {
+	if (GPIOA -> IDR &= 0x00000001)
+		return 1;
+	else 
+		return 0;
+	
+	
 }
 
 void initialiseLEDandButtonPorts(){
-	//GPIOA -> MODER = (GPIOA -> MODER & 0x000) | (0x01 << GPIO_MODER_MODER1_Pos);
-	GPIOD -> MODER  = 0x55000000 ;//| GPIOD -> MODER;
-	GPIOA -> MODER  = 0x00000000 ;//| GPIOA -> MODER;
-	GPIOD -> OTYPER = 0x00000000 ;//| GPIOD -> OTYPER;
-	GPIOA -> OTYPER = 0x00000000 ;//| GPIOA -> OTYPER;
-	GPIOD -> PUPDR  = 0x00000000 ;//| GPIOD -> PUPDR;
-	GPIOA -> PUPDR  = 0x00000000 ;//| GPIOA -> PUPDR;
-	
-//	GPIOD -> BSRR = 0X0000000C; //green LED
-//	GPIOD -> BSRR = 0X0000000D; //orange LED
-//	GPIOD -> BSRR = 0X0000000E; //red LED
-//	GPIOD -> BSRR = 0X0000000F; //blue LED
+	RCC->AHB1ENR = (RCC->AHB1ENR & 0x00000000) | 0x00000009;
+	GPIOD -> MODER  = (GPIOD->MODER & 0x00FFFFFF)|0x55000000 ;//| GPIOD -> MODER;
+	GPIOA -> MODER  = (GPIOA->MODER & 0x00000000) ;//| GPIOA -> MODER;
+	GPIOD -> OTYPER = (GPIOD->OTYPER & 0x00000000) ;//| GPIOD -> OTYPER;
+	GPIOD -> PUPDR  = (GPIOD->PUPDR & 0x00000000) ;//| GPIOD -> PUPDR;
+	GPIOA -> PUPDR  = (GPIOA->PUPDR & 0x00000002) ;//| GPIOA -> PUPDR;
+	GPIOD -> OSPEEDR = (GPIOD->OSPEEDR & 0x00000000) ;
 }
 
 int main (void) {
 SystemCoreClockUpdate();
 SysTick_Config(SystemCoreClock/4);
-//LED_Initialize();
 initialiseLEDandButtonPorts();
+SysTick_Handler ();
+while (1) {
+	if (GPIOA -> IDR &= 0x00000001){
+		if (LEDState == 0) GPIOD -> BSRR =   0xE0001000;
+		else if  (LEDState == 1) GPIOD -> BSRR =   0xD0002000;
+		else if  (LEDState == 2) GPIOD -> BSRR =   0xB0004000;
+		else GPIOD -> BSRR =   0x70008000;
+		}
 //while (1) {
-//if (LEDState == 0) LED_On(0), LED_Off(3);
-//else if  (LEDState == 1) LED_On(1), LED_Off(0);
-//else if  (LEDState == 2) LED_On(2), LED_Off(1);
-//else LED_On(3), LED_Off(2);
-//}
-//while (1) {
-	//buttonDetect ();
-	greenLED_invert () ;
-//}
-
+//	buttonDetect ();
+	//GPIOD -> BSRR = 0x0000F000;
+	//if (GPIOA -> IDR &= 0x00000001){
+		//buttonDetect ();
+	}
 }
