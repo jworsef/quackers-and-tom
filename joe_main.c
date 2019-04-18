82a16480bb6420146da1d494ed7e08a59d975605
jworsef <31925386+jworsef@users.noreply.github.com>
2019-04-18 12:22:15 +0100

    Update joe_main.c

--- joe_main.c
+++ joe_main.c
@@ -5,12 +5,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-int sahHighOn = 0;
-int sahLowOn = 0;	
+int sahOn = 0;	
 int LEDState;
+int refreshDisplay = 1;
+int mode = 0;
 float voltsSaHMax; //Max sample and hold value
 float voltsSaHMin; //Min sample and hold value
-float VoltsReadADC();
+float valueGivenByADC(); //Predefinition
 
 void initialiseLED(){
 	GPIOD -> MODER  = (GPIOD->MODER & 0x00FFFFFF)|0x55000000 ;//| GPIOD -> MODER;
@@ -20,37 +21,50 @@ void initialiseLED(){
 }
 
 void EXTI15_10_IRQHandler (void) { 
+/* Interrupt request handler for SW8 and SW9. Must be called before switches can work*/
 	if (GPIOE -> IDR &= 0x00004000){
 		EXTI -> PR = EXTI_PR_PR14;
-		//enter here what you want to do when SW8 is pressed and remove LEDState if wanted
-		sahHighOn = 1 -sahHighOn;
-		voltsSaHMax = VoltsReadADC();
 		
+		//Rotate through the 3 modes, V, I and R
+		mode++;	
+		if (mode == 3)
+		{
+			mode = 0;
+		}
 	}
 	else if (GPIOE -> IDR &= 0x00008000){
 		EXTI -> PR = EXTI_PR_PR15;
-		//enter here what you want to do when SW9 is pressed and remove LEDState if wanted
-		sahLowOn = 1 - sahLowOn;
-		voltsSaHMin = VoltsReadADC();
+		
+		
+		sahOn = 1 -sahOn;
+		voltsSaHMax = valueGivenByADC();
+		voltsSaHMin = valueGivenByADC();
 	}
 }
 
-void greenLED_only (){
-		GPIOD -> BSRR =   0xE0001000; //green LED
+void greenLED_only (void){
+//Sets the green LED on, all others off.
+		GPIOD -> BSRR =   0xE0001000; 
 }
 
-void orangeLED_only (){
-	GPIOD -> BSRR =   0xD0002000; // orange LED
+void orangeLED_only (void){
+//Sets the orange LED on, all others off.
+	GPIOD -> BSRR =   0xD0002000; 
 }
 
-void redLED_only (){
-	GPIOD -> BSRR =   0xB0004000; // red LED
+void redLED_only (void){
+//Sets the red LED on, all others off.
+	GPIOD -> BSRR =   0xB0004000; 
 }
 
-void blueLED_only (){
-	GPIOD -> BSRR =   0x70008000; // blue LED
+void blueLED_only (void){
+//Sets the blue LED on, all others off.
+
+	GPIOD -> BSRR =   0x70008000; 
 }
 
+/* I can't remember that this function is, and it seems to break things, but I don't want
+ * just kill it off yet. /
 void initialiseAF(){
 	//enabling the clock
 	RCC->AHB1ENR = (RCC->AHB1ENR & 0x00000000) | 0x00000005;
@@ -66,10 +80,10 @@ void initialiseAF(){
 	RCC->APB1ENR = (RCC->AHB1ENR & 0xCFFFFFFF) | 0x20000000;
 	//enabling DAC channel 1
 	DAC -> CR = (DAC->CR & 0xFFFFFFFE)|0x00000001;
-	
-}
+}*/
 
-float compReturnHigh(float newValue, float oldValue) //Compares values, returns highest
+float compReturnHigh(float newValue, float oldValue)
+/* Compares two float values. The highest of the two is returned. */
 {
 	if (newValue > oldValue)
 	{
@@ -81,7 +95,8 @@ float compReturnHigh(float newValue, float oldValue) //Compares values, returns 
 	}
 	}	
 	
-float compReturnLow(float newValue, float oldValue) //Compares values, returns lowest
+float compReturnLow(float newValue, float oldValue) 
+/* Compares two float values. The lowest of the two is returned. */
 {
 	if (newValue < oldValue)
 	{
@@ -94,6 +109,7 @@ float compReturnLow(float newValue, float oldValue) //Compares values, returns l
 }
 
 void ADC_Initialise (){
+/* Initialises the ADC. Must be called before any ADC operations are called.*/
 	RCC->APB2ENR = (RCC->APB2ENR & 0xFFFFFEFF) | 0x00000100;
 	
 	ADC1 -> SQR3  = (ADC1->SQR3 & 0xFFFFFFE0)|0x00000000E;
@@ -103,8 +119,9 @@ void ADC_Initialise (){
 	ADC1 -> CR1  = (ADC1->CR1 & 0xFFFFF7FF)|0x000000800;
 }
 
-float VoltsReadADC(){ //Reads the value from the ADC and returns a voltage	
-	
+float valueGivenByADC(){ 
+/* Reads the raw value from the ADC and calculates the value of the voltage across it. 	
+ * Returns the value as a floating-point number. */
 	ADC1 -> CR2  = (ADC1->CR2 & 0xBFFFFFFF)|0x40000000;
 	
 	if (ADC -> CSR &= 0x00000002){
@@ -116,105 +133,139 @@ float VoltsReadADC(){ //Reads the value from the ADC and returns a voltage
 	}
 	else
 	{
-		return VoltsReadADC(); //And hope it doesn't fail more than once
+		return valueGivenByADC(); //And hope it doesn't fail more than once
 	}
 }
 
-char* arvStringfromVolt(float volts) //returns an autoranged value as a string from a given voltage
+char*	arvStringfromValue(float volts, char unit) 
+/* Takes a value in unit volts and auto-ranges it to mV. Then converts it to a string,
+ * with the value to 4 s.f. followed by the correct unit. It is returned a pointer to  
+ * a string. */
 {
 	char stringOut[10];
 	char* ptrStringOut = stringOut;
-	/*if (volts <= 0) 
-	{
-		
-		// always 4 s.f.
-		if (volts >= 0.1)
-		{
-			snprintf (stringOut, 10, "%.1fmV ", volts*1000);
-		}
-		else
-		{
-			if (volts >= 0.01)
-			{
-				snprintf (stringOut, 10, "%.2fmV ", volts*1000);
-			}
-			
-			else
-			{
-				snprintf (stringOut, 10, "%.3fmV ", volts*1000);
-			}
-		}
-	}
-	else {*/
+
 	if (volts >= 1)
 	{
-		snprintf (stringOut, 10, "%.3fV ", volts);
+		snprintf (stringOut, 10, "%.3f", volts);
 	}
 	else
 	{
 		if (volts >= 0.1)
 		{
-			snprintf (stringOut, 10, "%.1fmV ", volts*1000);
+			snprintf (stringOut, 10, "%.1fm", volts*1000);
 		}
 		else 
 		{
 			if (volts >= 0.01)
 			{
-				snprintf (stringOut, 10, "%.2fmV ", volts*1000);
+				snprintf (stringOut, 10, "%.2fm", volts*1000);
 			}
 			else 
 			{
-				snprintf (stringOut, 10, "%.3fmV ", volts*1000);
+				snprintf (stringOut, 10, "%.3fm", volts*1000);
 			}
 		}
 	}
-		//}
-	// TEST: PB_LCD_WriteString(stringOut, 10);
+	strcat(stringOut, &unit);
+	strcat(stringOut, " ");
+  return ptrStringOut;
+}
+
+char* arvStringfromAmp(float amps) 
+/* Takes a value in unit volts and auto-ranges it to mV. Then converts it to a string,
+ * with the value to 4 s.f. followed by the correct unit. It is returned a pointer to  
+ * a string. */
+{
+	char stringOut[10];
+	char* ptrStringOut = stringOut;
+
+	if (amps >= 1)
+	{
+		snprintf (stringOut, 10, "%.3fA ", amps);
+	}
+	else
+	{
+		if (amps >= 0.1)
+		{
+			snprintf (stringOut, 10, "%.1fmA ", amps*1000);
+		}
+		else 
+		{
+			if (amps >= 0.01)
+			{
+				snprintf (stringOut, 10, "%.2fmA ", amps*1000);
+			}
+			else 
+			{
+				snprintf (stringOut, 10, "%.3fmA ", amps*1000);
+			}
+		}
+	}
 	
   return ptrStringOut;
 }
 
-void displayVoltage(float voltsADC) //Writes a voltage to the LCD
+char unitOfMode()
+/* Reads the current mode of operation, returns the correct unit of measurement */
+{
+	switch(mode)
+	{
+		case 0	:
+			return 'V';
+		case 1	:
+			return 'A';
+		case 2	:
+			return (char)222;
+		}
+}
+
+void displayValue(float voltsADC) 
+/* Takes a volt value and displays it on the LCD */
 {
 	char LCD_out[15];
 	char LCD_minmax[15];
-	
+	char unit = unitOfMode();
 	//Sample and hold
 	
-	if (sahHighOn == 1)
+	if (sahOn == 1)
 	{
 		voltsSaHMax = compReturnHigh(voltsADC, voltsSaHMax);
-	}
-	if (sahLowOn == 1)
-	{
 		voltsSaHMin = compReturnLow(voltsADC, voltsSaHMin);
 	}
 	
 	//Auto-ranging 
-	strcpy(LCD_out, arvStringfromVolt(voltsADC));
+	strcpy(LCD_out, arvStringfromValue(voltsADC, unit));
 	
-	//LCD write out
-	PB_LCD_Clear();
-	//PB_LCD_GoToXY(0, 0);
 
+	PB_LCD_Clear();
 	PB_LCD_WriteString(LCD_out, 16);
 	
-	if (sahHighOn == 1 && sahLowOn == 1)
+	if (sahOn == 1)
 	{
-		strcpy(LCD_minmax, arvStringfromVolt(voltsSaHMin));
-  	strncat(LCD_minmax, arvStringfromVolt(voltsSaHMax), 16);
+		strcpy(LCD_minmax, arvStringfromValue(voltsSaHMin, unit));
+		strncat(LCD_minmax, arvStringfromValue(voltsSaHMax, unit), 16);
 		PB_LCD_GoToXY(0, 1);
 		PB_LCD_WriteString(LCD_minmax, 16);
 	}
+
 
 }
  
  void SysTick_Handler (void) 
 { 
+	refreshDisplay = 1;
 }
  
-int main (void) {
-	
+int menuModeSelect (void)
+{
+			PB_LCD_WriteChar((char)197);
+			PB_LCD_WriteChar((char)198);
+}
+
+int main (void) 
+{
+	//Something to do with interrupts, ask Tom, not me.
 	RCC -> AHB1ENR = (RCC->APB1ENR & 0xFFFFFFE7) | 0x00000018;
 		
 	RCC -> APB2ENR = (RCC -> APB2ENR & 0xFFFFBFFF) | 0x00004000;
@@ -235,6 +286,7 @@ int main (void) {
 	initialiseLED();
 	EXTI15_10_IRQHandler ();
 	
+	// My code starts here
 	SystemCoreClockUpdate();
 	//initialiseAF();
 	ADC_Initialise ();
@@ -242,15 +294,12 @@ int main (void) {
 	LED_Initialize();
 	SysTick_Config(SystemCoreClock/2); 
 	
-	//displayVoltage(VoltsReadADC());
-	
 	while(1)
 	{
-		int i = 0;	
-		displayVoltage(VoltsReadADC());
-		while(i<1000000)
+		if (refreshDisplay == 1)
 		{
-			i=i+1;
+			displayValue(valueGivenByADC());
+			refreshDisplay = 0;
 		}
 	}
-} 
+} 	 	
