a41ee976b06be1766bae01afa8b3f45728f27103
jworsef <31925386+jworsef@users.noreply.github.com>
2019-04-25 16:52:17 +0100

    Update mergedCode.c

--- mergedCode.c
+++ mergedCode.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-
+#define nullptr NULL
 int sahOn = 0;	
 int LEDState;
 int refreshDisplay = 1;
@@ -24,7 +24,7 @@ void EXTI15_10_IRQHandler (void) {
 	if (GPIOE -> IDR &= 0x00004000){
 		EXTI -> PR = EXTI_PR_PR14;
 		
-		//Rotate through the 3 modes, V, I and R
+		//Rotate through the 6 modes, V, I, little R, big R, frequency and store/recall.
 		mode++;	
 		if (mode == 5)
 		{
@@ -57,6 +57,27 @@ void EXTI15_10_IRQHandler (void) {
 	}
 }
 
+void waitForSeconds(int seconds){
+	systemTime = 0;
+	int currentMode = mode;
+	while(systemTime != seconds && currentMode == mode); 
+}
+
+
+void beepInit()
+{
+	RCC -> AHB1ENR = (RCC->APB1ENR & 0xFFFFFFFD) | 0x00000002;
+	GPIOB -> MODER = (GPIOB -> MODER & 0xFFFFFF3F) | 0x00000040;
+	GPIOB -> OTYPER = (GPIOB -> OTYPER & 0xFFFFFFF7);
+	GPIOB -> PUPDR = (GPIOB -> PUPDR & 0xFFFFFF3F) | 0x00000080;
+}
+
+void beep()
+{
+	GPIOB -> BSRR = (GPIOB -> BSRR & 0xFFFFFFF7) | 0x00000008;
+	waitForSeconds(3);
+	GPIOB -> BSRR = (GPIOB -> BSRR & 0xFFF7FFFF) | 0x00080000;
+}
 float compReturnHigh(float newValue, float oldValue){
 /* Compares two float values. The highest of the two is returned. */
 	if (newValue > oldValue)
@@ -123,9 +144,12 @@ char*	arvStringfromValue(float volts, char unit){
 	//Mode of operation checking
 	if (unit == 'V') //Volts
 	{
-		outputValue = ((volts - 1.5f)/0.15f - 0.6f);
+		outputValue = ((volts - 1.5f)/0.15f - 0.2f);
 	}
-	
+	else if (unit == 'A') //Amps
+	{
+		outputValue = ((volts - 1.5f)/0.15f)/10.05f;
+	}
 	else if (unit == (char)222) //Ohms
 	{
 		outputValue = (volts*995.8f)/(5-volts);
@@ -173,43 +197,14 @@ char unitOfMode(){
 			return 'A';
 		case 2	:
 			return (char)222;
+		case 3	:
+			return 'H';
 		}
 	//If mode is ever equal to or greater than 3, you've broken something
 	return 'X';
 }
 
-void displayValue(float voltsADC){
-/* Takes a volt value from ADC and displays it on the LCD 
- * Only performs string and LCD operations, all convertions and calculates happen in arvStringfromValue*/
 
-	char LCD_out[15];
-	char LCD_minmax[15];
-	char unit = unitOfMode();
-	//Sample and hold
-	
-	if (sahOn == 1)
-	{
-		voltsSaHMax = compReturnHigh(voltsADC, voltsSaHMax);
-		voltsSaHMin = compReturnLow(voltsADC, voltsSaHMin);
-	}
-	
-	//Pass the volt value to arvStringfromValue() and collect the string it returns.
-	strcpy(LCD_out, arvStringfromValue(voltsADC, unit));
-	
-
-	PB_LCD_Clear();
-	PB_LCD_WriteString(LCD_out, 16);
-	
-	if (sahOn == 1)
-	{
-		strcpy(LCD_minmax, arvStringfromValue(voltsSaHMin, unit));
-		strncat(LCD_minmax, arvStringfromValue(voltsSaHMax, unit), 16);
-		PB_LCD_GoToXY(0, 1);
-		PB_LCD_WriteString(LCD_minmax, 16);
-	}
-
-
-}
  
  void SysTick_Handler (void){
 /*Many times a second, this is called*/
@@ -224,7 +219,7 @@ void menuModeSelect (void){
 }
 
 void initialiseAF(){
-	/*Something to do with initialising the ADC*/
+	/*Something to do with initiliasing the ADC*/
 	//enabling the clock
 	RCC->AHB1ENR = (RCC->AHB1ENR & 0xFFFFFFF9) | 0x00000005;
 	//Timer 1 external trigger and DAC out 1 enable
@@ -242,273 +237,50 @@ void initialiseAF(){
 	
 }
 
-
-void Vfunc(struct ValuesList* list)
-{
-	//Voltmeter Mode and accuracy (displayed for 3sec)
+void displayModeMessage(){
 	PB_LCD_Clear();
-	PB_LCD_WriteString("Voltmeter",14);
-	PB_LCD_GoToXY(0, 1);
-	PB_LCD_WriteString("Accuracy", 15); //ADD THE VALUE
-	
-	//wait statement
-	wait3sec();	
-
-	int myFlag=0;
-	
-	while(mode==0)
-	{
-		if (refreshDisplay == 1)
-		{
-			//function that takes a volt value from 0-3 Volt
-			//and returns a value from -10 to 10 V
-			displayValue(valueGivenByADC());
-			
-			//if the user wants to store a value store the value
-			if(store==1)
-			{	
-				store=0;
-				storeValue(list,arvStringfromValue(valueGivenByADC(),unitOfMode()));
-			}
-			
-			//if the user tries to measure a value close to the max beep thrice (:P three times)
-			if(abs(valueGivenByADC())>9.5 && myFlag==0)
-			{
-				
-				//briefly display what the problem is ?
-				PB_LCD_Clear();
-				PB_LCD_WriteString("Value measured",16);
-				PB_LCD_GoToXY(0, 1);
-				PB_LCD_WriteString("is close to max", 16);
-				
-				beep();
-				//do we need other wait ?			
-
-				//set the flag high
-				myFlag=1;
-			}
-			//reset the flag when the value drops lower 
-			if(abs(valueGivenByADC())<9.3 && myFlag==1) 
-			{
-				//set the flag low
-				myFlag=0;
-			}
-			refreshDisplay = 0;
-		}
+	if (mode == 0){
+			PB_LCD_WriteString("Voltmeter",16);
+			//PB_LCD_GoToXY(0, 1);
+		  //PB_LCD_WriteString("Accuracy:", 16); //ADD THE VALUE
 	}
-}
-
-//do we wanna beep inside here ?
-void Rfunc(struct ValuesList* list)
-{
-	//Ohmmeter Mode, what mode is at and accuracy (displayed for 3sec)
-	PB_LCD_Clear();
-	PB_LCD_WriteString("Ohmmeter",13);
-	PB_LCD_GoToXY(0, 1);
-	PB_LCD_WriteString("Accuracy", 16); //ADD THE VALUE
-	
-	//wait statement
-	wait3sec();
-	
-	while(mode==1)
-	{
-		if (refreshDisplay == 1)
-		{
-			while(ohmMode==0)
-			{
-				//we will check A pin and display at lcd the value we think the resistor has
-				displayValue(valueGivenByADC()); //both A and B use the same
-				
-				//if the user wants to store a value store the value
-				if(store==1)
-				{
-					store=0;
-					storeValue(list,arvStringfromValue(valueGivenByADC(),unitOfMode()));
-				}
-			}
-			while(ohmMode==1)
-			{
-				//we will check B pin and display at lcd the value we think the resistor has
-				displayValue(valueGivenByADC()); //both A and B use the same
-				
-				//if the user wants to store a value store the value
-				if(store==1)
-				{
-					store=0;
-					storeValue(list,arvStringfromValue(valueGivenByADC(),unitOfMode()));
-				}
-			}
-		refreshDisplay = 0;
-		}
+	else if (mode == 1){
+			PB_LCD_WriteString("Ammeter",16);
+			//PB_LCD_GoToXY(0, 1);
+			//PB_LCD_WriteString("Accuracy:", 16); //ADD THE VALUE
 	}
-}	
-
-void Ifunc(struct ValuesList* list)
-{
-	//current mode and accuracy (displayed for 3sec)
-	PB_LCD_Clear();
-	PB_LCD_WriteString("Ammeter",12);
-	PB_LCD_GoToXY(0, 1);
-	PB_LCD_WriteString("Accuracy", 16); //ADD THE VALUE
-	
-	//wait statement
-	wait3sec();
-
-///for calculating only low current else we need to add modes
-	while(mode==2)
-	{
-		if (refreshDisplay == 1)
-		{
-			//function that takes a voltage value for low current
-			//and returns the current value
-			displayValue(valueGivenByADC());
-			
-			//if the user wants to store a value store the value
-			if(store==1)
-			{
-				store=0;
-				storeValue(list,arvStringfromValue(valueGivenByADC(),unitOfMode()));
-			}
-			
-			//if the user tries to measure a value close to the max beep thrice (:P three times)
-			if(abs(valueGivenByADC())>0.95 && myFlag==0)
-			{			
-				//briefly display what the problem is ?
-				PB_LCD_Clear();
-				PB_LCD_WriteString("Value measured",16);
-				PB_LCD_GoToXY(0, 1);
-				PB_LCD_WriteString("is close to max", 16);
-				
-				beep();
-
-				//set the flag high
-				myFlag=1;
-			}
-			//reset the flag when the value drops lower 
-			if(abs(valueGivenByADC())<0.93 && myFlag==1)
-			{
-				//set the flag low
-				myFlag=0;
-			}
-		refreshDisplay=0;
-		}
+	else if (mode == 2){
+			PB_LCD_WriteString("Resistance",16);
+			//PB_LCD_GoToXY(0, 1);
+			//PB_LCD_WriteString("Accuracy", 16); //ADD THE VALUE
 	}
-}
-
-void Stored_func(struct ValuesList* list)
-{
-	//current mode and number of stored values(? no) (displayed for 3sec)
-	PB_LCD_Clear();
-	PB_LCD_WriteString("Stored Values",13);
-	
-	//wait statement
-	wait3sec();
-
-	struct ValuesNode* displayedNode=list->head; //allways display the first node first
-	//reset buttons
-	nextNode=0;
-	prevNode=0;
-	while(mode==4)
-	{
-		if (refreshDisplay == 1)
-		{
-			//if list is empty let the user know
-			//else let the user interact with the list
-			if (list->size==0)
-			{
-				PB_LCD_Clear();
-				PB_LCD_WriteString("Nothing Stored",15);
-			}
-			else
-			{
-				printLCD(displayedNode);
-				if(nextNode==1 && list->size!=1)
-				{
-					nextNode=0;
-					moveToNextNode(displayedNode); // this fucntion is really simple do we really need it ?
-				}
-				if(prevNode==1 && list->size!=1)
-				{
-					prevNode=0;
-					moveToPrevNode(displayedNode); // this fucntion is really simple do we really need it ?
-				}
-			}
-		refreshDisplay = 0;
-		}
+	else if (mode == 3){
+			PB_LCD_WriteString("Frequency",16);
+			//PB_LCD_GoToXY(0, 1);
+			//PB_LCD_WriteString("Accuracy", 16); //ADD THE VALUE
 	}
-	nextNode=0;
-	prevNode=0;
-	store=0; // so that if the store button is pressed inside here it will be reseted automatically before the next function that actually uses it
-}
-
-void Ffunc(struct ValuesList* list)
-{
-	//current mode and accuracy (3 sec)
-	PB_LCD_Clear();
-	PB_LCD_WriteString("Frequency Meter",15);
-	PB_LCD_GoToXY(0, 1);
-	PB_LCD_WriteString("Accuracy", 16); //ADD THE VALUE
-	
-	wait3sec();
-	
-	while(mode==3)
-	{
-		if (refreshDisplay == 1)
-		{
-			//rising edge/time = freq 
-			//display it 
-			
-			//if the user wants to store a value store the value
-			if(store==1)
-			{
-				store=0;
-				storeValue(list,arvStringfromValue(valueGivenByADC(),unitOfMode()));
-			}
-			
-			//if the user tries to measure a value close to the max beep thrice (:P three times)
-			if(abs(valueGivenByADC())>9.5 && myFlag==0)
-			{
-				//briefly display what the problem is ?
-				//this code has been reapeted maybe make a func for it ?
-				PB_LCD_Clear();
-				PB_LCD_WriteString("Value measured",16);
-				PB_LCD_GoToXY(0, 1);
-				PB_LCD_WriteString("is close to max", 16);
-				
-				beep();
-				
-				//set the flag high
-				myFlag=1;
-			}
-			//reset the flag when the value drops lower 
-			if(abs(valueGivenByADC())<9.3 && myFlag==1)
-			{
-				//set the flag low
-				myFlag=0;
-			}
-		refreshDisplay=0;	
-		}
+	else if (mode == 4){
+			PB_LCD_WriteString("Stored values",16);
+			//PB_LCD_GoToXY(0, 1);
+			//PB_LCD_WriteString("Accuracy", 16); //ADD THE VALUE
 	}
+	waitForSeconds(3);
 }
 
 ///////  lvl 3 requirement  //////
-
 /// STRUCTURE TO STORE VALUES
-
 struct ValuesList
 {
 	struct ValuesNode* head;
 	int size;
-}
-
+};
 struct ValuesNode
 {
-	char stringStored[cs];
+	char stringStored[16];
 	int readingnumber;
 	struct ValuesNode* next;
 	struct ValuesNode* prev;
-}
-
+};
 struct ValuesNode* nodeConstructor(char string[],int readingnumber)
 {
 	struct ValuesNode* newNode;
@@ -525,12 +297,10 @@ struct ValuesNode* nodeConstructor(char string[],int readingnumber)
 	//return constructed node
 	return newNode;
 }
-
 void nodeDestructor(struct ValuesNode* node)
 {
 	free(node);
 }
-
 struct ValuesList* listConstructor()
 {
 	struct ValuesList* newList;
@@ -542,27 +312,25 @@ struct ValuesList* listConstructor()
 	//return constructed list
 	return newList;
 }
-
 /// FUNCTIONS FOR CREATING LIST
 /* We will have a button that stores a new value once pressed
 every mode (I,R,V) will pass a different unit (I->Amp,R->Ohm,V->Volt,etc.)
 we want this function to add the new value at the start of the list and when we print 
 it we are going to print it from the last value we stored having the ability to go to the next and prev
 we can also go from the first to last because it is going to be a doubly linked circular list */
-
 void storeValue(struct ValuesList* list, char string[])
 {
 	struct ValuesNode* newNode;
 	struct ValuesNode* tmpNode; //node to be destructed if needed
 	
 	//incrementing the size of the list
-	ValuesList->size++;
+	list->size++;
 	
 	//setting the new node
-	newNode=nodeConstructor(string,ValuesList->size);
+	newNode=nodeConstructor(string,list->size);
 	
 	//if list is empty
-	if(ValuesList->head==nullptr)
+	if(list->head==nullptr)
 	{
 		list->head=newNode;
 		newNode->next=newNode;	//to help with 
@@ -586,7 +354,7 @@ void storeValue(struct ValuesList* list, char string[])
 	
 	//if size of list is bigger than 10 then delete the first value that was stored
 	//so that the heap and the stack will not crash 
-	if(ValuesList->size>10)
+	if(list->size>10)
 	{
 		//setting tmpNode to the node that was saved earlier
 		tmpNode=newNode->prev;
@@ -601,9 +369,8 @@ void storeValue(struct ValuesList* list, char string[])
 	//print at LCD "VALUE STORED"
 	PB_LCD_Clear();
 	PB_LCD_WriteString("Value Stored",16);
-	wait1sec();
+	waitForSeconds(1);
 }
-
 void printLCD(struct ValuesNode* DisplayedNode)
 {
 	//char lcd1[20];
@@ -617,51 +384,116 @@ void moveToNextNode(struct ValuesNode* DisplayedNode)
 {
 	DisplayedNode=DisplayedNode->next;
 }
-
 void moveToPrevNode(struct ValuesNode* DisplayedNode)
 {
 	DisplayedNode=DisplayedNode->prev;
 }
 
-//////FUNCTION TO CREATE A WAVE NEEDED FOR THE BEEPER
-void beepInit()
+void Stored_func(struct ValuesList* list)
 {
-	RCC -> AHB1ENR = (RCC->APB1ENR & 0xFFFFFFFD) | 0x00000002;
-	GPIOB -> MODER = (GPIOB -> MODER & 0xFFFFFF3F) | 0x00000040;
-	GPIOB -> OTYPER = (GPIOB -> OTYPER & 0xFFFFFFF7);
-	GPIOB -> PUPDR = (GPIOB -> PUPDR & 0xFFFFFF3F) | 0x00000080;
-}
-
-void beep()
-{
-	GPIOB -> BSRR = (GPIOB -> BSRR & 0xFFFFFFF7) | 0x00000008;
-	wait3sec();
-	GPIOB -> BSRR = (GPIOB -> BSRR & 0xFFF7FFFF) | 0x00080000;
-}
-
-// tom said we cant work at the systick by dividing with values really big like 60M
-// so maybe have a function that will run many smaller systick till we achieve the 3 sec 
-// we want
-void wait1sec()
-{
-	int counter=0;
-	//refreshDisplay changes at a frequency of 8mega
-	//so we need to just count 8 m times to reach a second (?)
-	while (counter<2)
+	//current mode and number of stored values(? no) (displayed for 3sec)
+	PB_LCD_Clear();
+	PB_LCD_WriteString("Stored Values",13);
+	
+	//wait statement
+	waitForSeconds(3);
+	struct ValuesNode* displayedNode=list->head; //allways display the first node first
+	//reset buttons
+	nextNode=0;
+	prevNode=0;
+	while(mode==4)
 	{
-		if(refreshDisplay==1)
+		if (refreshDisplay == 1)
 		{
-			counter++;
-			refreshDisplay=0;
+			//if list is empty let the user know
+			//else let the user interact with the list
+			if (list->size==0)
+			{
+				PB_LCD_Clear();
+				PB_LCD_WriteString("Nothing Stored",16);
+			}
+			else
+			{
+				printLCD(displayedNode);
+				if(nextNode==1 && list->size!=1)
+				{
+					nextNode=0;
+					moveToNextNode(displayedNode); // this fucntion is really simple do we really need it ?
+				}
+				if(prevNode==1 && list->size!=1)
+				{
+					prevNode=0;
+					moveToPrevNode(displayedNode); // this fucntion is really simple do we really need it ?
+				}
+			}
+		refreshDisplay = 0;
 		}
 	}
-
+	nextNode=0;
+	prevNode=0;
+	store=0; // so that if the store button is pressed inside here it will be reseted automatically before the next function that actually uses it
 }
-void wait3sec()
-{
-	wait1sec();
-	wait1sec();
-	wait1sec();
+
+void displayValue(struct ValuesList* list){
+/* Takes a volt value from ADC and displays it on the LCD 
+ * Only performs string and LCD operations, all convertions and calculates happen in arvStringfromValue*/
+
+	char LCD_out[15];
+	char LCD_minmax[15];
+	char unit = unitOfMode();
+	float voltsADC = valueGivenByADC();
+	//Sample and hold
+	
+	if (sahOn == 1)
+	{
+		voltsSaHMax = compReturnHigh(voltsADC, voltsSaHMax);
+		voltsSaHMin = compReturnLow(voltsADC, voltsSaHMin);
+	}
+	
+	//Pass the volt value to arvStringfromValue() and collect the string it returns.
+	strcpy(LCD_out, arvStringfromValue(voltsADC, unit));
+	
+
+	PB_LCD_Clear();
+	PB_LCD_WriteString(LCD_out, 16);
+	
+	if (sahOn == 1)
+	{
+		strcpy(LCD_minmax, arvStringfromValue(voltsSaHMin, unit));
+		strncat(LCD_minmax, arvStringfromValue(voltsSaHMax, unit), 16);
+		PB_LCD_GoToXY(0, 1);
+		PB_LCD_WriteString(LCD_minmax, 16);
+	}
+	
+	//Store value
+	if(store==1)
+	{	
+		store=0;
+		storeValue(list, arvStringfromValue(voltsADC, unit));
+	}
+	
+	//if the user tries to measure a value close to the max beep thrice (:P three times)
+	int myFlag=0;
+	if(fabs(valueGivenByADC())>9.5 && myFlag==0)
+	{
+		
+		//briefly display what the problem is ?
+		PB_LCD_Clear();
+		PB_LCD_WriteString("Value measured",16);
+		PB_LCD_GoToXY(0, 1);
+		PB_LCD_WriteString("is close to max", 16);
+		
+	//	beep();
+		//do we need other wait ?			
+		//set the flag high
+		myFlag=1;
+	}
+	//reset the flag when the value drops lower 
+	if(fabs(valueGivenByADC())<9.3 && myFlag==1) 
+	{
+		//set the flag low
+		myFlag=0;
+	}
 }
 
 int main (void) {
@@ -670,19 +502,17 @@ int main (void) {
 		
 	RCC -> APB2ENR = (RCC -> APB2ENR & 0xFFFFBFFF) | 0x00004000;
 
-	GPIOE -> MODER = (GPIOE -> MODER & 0x0000FFFF);
+	GPIOE -> MODER = (GPIOE -> MODER & 0x0FFFFFFF);
 		
-	GPIOE -> PUPDR = (GPIOE -> PUPDR & 0x0000FFFF) | 0xAAAA0000;
+	GPIOE -> PUPDR = (GPIOE -> PUPDR & 0x0FFFFFFF) | 0xA0000000;
 		
-	SYSCFG -> EXTICR[2] = (SYSCFG -> EXTICR[2] & 0xFFFF0000) | 0x00004444;
+	SYSCFG -> EXTICR[3] = (SYSCFG -> EXTICR[3] & 0xFFFF00FF) | 0x00004400;
 	
-	SYSCFG -> EXTICR[3] = (SYSCFG -> EXTICR[3] & 0xFFFF0000) | 0x00004444;
-	
-	EXTI -> IMR = (EXTI -> IMR & 0xFFFF00FF) |	0x0000FF00;
+	EXTI -> IMR = (EXTI -> IMR & 0xFFFF3FFF) |	0x0000C000;
 		
-	EXTI -> RTSR = (EXTI -> RTSR & 0xFFFF00FF) |	0x0000FF00;
+	EXTI -> RTSR = (EXTI -> RTSR & 0xFFFF3FFF) |	0x0000C000;
 
-	EXTI -> FTSR = (EXTI -> FTSR & 0xFFFF00FF);
+	EXTI -> FTSR = (EXTI -> FTSR & 0xFFFF3FFF);
 	
 	// My code starts here
 	SystemCoreClockUpdate();
@@ -692,17 +522,31 @@ int main (void) {
 	LED_Initialize();
 	beepInit();
 	SysTick_Config(SystemCoreClock/2); 
-	
+	int currentMode = 11;
 	NVIC_EnableIRQ(EXTI15_10_IRQn);
 	EXTI15_10_IRQHandler ();
-	struct ValuesList* list=listConstructor;
+	
+	struct ValuesList* list=listConstructor();
+	list->size=0;
+	
 	while(1)
+	{
+		if (refreshDisplay == 1)
 		{
-			
-			Vfunc(list); //mode 0
-			Rfunc(list); //mode 1
-			Ifunc(list); //mode 2
-			Ffunc(list); //mode 3
-			Stored_func(list); //mode 4
+			if (currentMode != mode)
+			{
+				displayModeMessage();
+			}
+			if (mode < 4)
+			{
+				displayValue(list);
+			}
+			else 
+			{
+				Stored_func(list);
+			}
+			currentMode = mode;
+			refreshDisplay = 0;
 		}
-} 
+	}
+} 	 	
